fbcd0af1c9276fb1f389a5a9595cde1de38215b4
bstjohn <bstjohn@pdx.edu>
2015-01-17 14:23:08 -0800

    Edited url configurations

--- bpe_capstone/urls.py
+++ bpe_capstone/urls.py
@@ -5,8 +5,10 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'bpe_capstone.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^$', 'django.contrib.auth.views.login'),
     url(r'^login/$', 'django.contrib.auth.views.login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout'),
     url(r'^info/', include('info.urls', namespace="info")),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^dashboard/', include('dashboard.urls', namespace="dashboard"))
 )
