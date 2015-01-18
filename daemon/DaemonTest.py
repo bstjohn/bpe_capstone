cfecfcfb173178cd3e2f3a701e0c59f15d4a2952
Daniel E. Wilson <danw@bureau-13.org>
2015-01-17 20:50:49 -0800

    Changed server to handle multiline input.

--- daemon/DaemonTest.py
+++ daemon/DaemonTest.py
@@ -1,4 +1,49 @@
 #!/usr/bin/python2.7
 #
-#  File:  DaemonTest.py
+#  File:    DaemonTest.py
+#  Author:  Daniel E. Wilson
 #
+#  This is a test program to see if the query daemon is correctly
+#  responding to requests.
+#
+import sys
+import socket
+import select
+
+# Set the server and host name to work with.
+host='localhost'
+port=4242
+
+# Create the socket.
+s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Set the test text.
+txt="This is a test.\nThis is another test.\n"
+
+try:
+    # Now connect to the server.
+    s.connect((host,port))
+    s.sendall(txt)
+
+    # Get read all of the response.
+    s.setblocking(0)
+    response=''
+    while True:
+        reading, writing, error = select.select([s],[],[],60)
+        if reading:
+            data = s.recv(1024)
+            if not data:
+                break
+            else:
+                response = response + data
+
+
+    # Show the user what was send and recieved.
+    print "Sent: {}".format(txt)
+    print "Received: {}".format(response)
+
+finally:
+    s.close()
+
+# End the program.
+sys.exit(0)
--- daemon/QueryDaemon.py
+++ daemon/QueryDaemon.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python2.7
 #
 #  File:    QueryDaemon.py
 #  Author:  Daniel E. Wilson
@@ -5,3 +6,42 @@
 #  This is the Server daemon that holds performs the actual queries
 #  once a user has initiated them from the Web interface.
 #
+#  This program should only be used with Python 2.7
+#
+import sys
+import select
+import SocketServer
+
+class QueryHandler(SocketServer.BaseRequestHandler):
+    "QueryHandler class for the query daemon."
+
+    def handle(self):
+        "Request handler function."
+        size = 4096
+
+        # Data read loop.
+        response=''
+        while True:
+            data = self.request.recv(size)
+            if len(data)<size:
+                response = response + data
+                break
+            else:
+                response = response + data
+
+        # Send the data back to the client.
+        self.request.sendall(response)
+
+# Start the server.
+if __name__=='__main__':
+
+    # Set the hostname and port numbers.
+    host = 'localhost'
+    port = 4242
+
+    # Create and run the server.
+    server = SocketServer.TCPServer((host,port), QueryHandler)
+    server.serve_forever()
+
+    # End the program.
+    sys.exit(0)
