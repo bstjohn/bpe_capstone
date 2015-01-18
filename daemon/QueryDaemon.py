a9b25b3be67d153bc480a92e53bcd8e75e2c693a
Daniel E. Wilson <danw@bureau-13.org>
2015-01-17 20:48:18 -0800

    Added first crude attempt at a daemon to handle the queries.

---
+++ daemon/QueryDaemon.py
@@ -0,0 +1,7 @@
+#
+#  File:    QueryDaemon.py
+#  Author:  Daniel E. Wilson
+#
+#  This is the Server daemon that holds performs the actual queries
+#  once a user has initiated them from the Web interface.
+#
