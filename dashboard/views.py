ef5f7a8fd7f3fe3ba4e6a13df1be82b62277151f
bstjohn <bstjohn@pdx.edu>
2015-01-20 15:56:11 -0800

    Made the dashboard page require a valid login.

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
+from django.contrib.auth.decorators import login_required
 
+
+@login_required
 def dashboard(request):
     username = None
     if request.user.is_authenticated():
