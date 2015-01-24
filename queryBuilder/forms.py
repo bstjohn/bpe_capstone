34b67ce14a1e48eac7d51163aea3f21fcb6656be
bstjohn <bstjohn@pdx.edu>
2015-01-23 19:22:54 -0800

    Created a calendar icon and date picker for dates

--- queryBuilder/forms.py
+++ queryBuilder/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.forms.widgets import SplitDateTimeWidget, SelectMultiple
+from django.forms.widgets import SplitDateTimeWidget, SelectMultiple, TimeInput, DateInput
 
 
 STATION_CHOICES = (('station1', 'Station1'),
@@ -9,7 +9,12 @@ STATION_CHOICES = (('station1', 'Station1'),
 
 # The query form attributes
 class QueryForm(forms.Form):
-    query_name = forms.CharField(label='Name Your Query', max_length=100)
-    start_date_time = forms.DateTimeField(label='Start Date/Time', widget=SplitDateTimeWidget)
-    end_date_time = forms.DateTimeField(label='End Date/Time', widget=SplitDateTimeWidget)
+    query_name = forms.CharField(label='Name of Your Query', max_length=100,
+                                 widget=forms.TextInput(attrs={'placeholder': 'Name'}))
+    start_date = forms.DateField(label='Start Date', widget=DateInput(attrs={'placeholder': 'mm/dd/yyyy',
+                                                                             'class': 'datepicker'}))
+    start_time = forms.TimeField(label='Start Time', widget=TimeInput(attrs={'placeholder': 'HH:mm:ss (24-hour)'}))
+    end_date = forms.DateField(label='End Date', widget=DateInput(attrs={'placeholder': 'mm/dd/yyyy',
+                                                                         'class': 'datepicker'}))
+    end_time = forms.TimeField(label='End Time', widget=TimeInput(attrs={'placeholder': 'HH:mm:ss (24-hour)'}))
     stations = forms.CharField(label='Stations', widget=forms.Select(choices=STATION_CHOICES))
