5c4d725ddd9558d353aae1ef1ba42c4afa217b38
Zaynab Alattar <zaalatta@cat.pdx.edu>
2015-01-29 16:27:52 -0800

    Added starttls() and verified sending mail works.

--- Email Notification/sendEmailNotification.py
+++ Email Notification/sendEmailNotification.py
@@ -6,7 +6,7 @@ from email.mime.text import MIMEText
 
 # Open a plain text file for reading.  For this example, assume that
 # the text file contains only ASCII characters.
-fp = open(message.txt, 'rb')
+fp = open("message.txt", 'rb')
 # Create a text/plain message
 msg = MIMEText(fp.read())
 fp.close()
@@ -14,12 +14,15 @@ fp.close()
 # me == the sender's email address (will need to get an email address from BPA)
 # you == the recipient's email address
 msg['Subject'] = 'Notification of Query Result'
-msg['From'] = me
-msg['To'] = you
+msg['From'] = 'Zaynab'
+msg['To'] = 'Zaynab'
 
-# Will need ask about this
+# Will need to ask about this
 # Send the message via our own SMTP server, but don't include the
 # envelope header.
-s = smtplib.SMTP('localhost')
-s.sendmail(me, [you], msg.as_string())
-s.quit()
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.set_debuglevel(1)
+server.ehlo()
+server.starttls()
+server.login('zalattar@pdx.edu', '')
+server.sendmail('zalattar@pdx.edu', 'zalattar@frontier.com', msg.as_string())
