2ec6652767202014f2270808600ef968406a211b
bstjohn <bstjohn@pdx.edu>
2015-01-31 13:08:00 -0800

    modified admin site to include queries and dashboards

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from dashboard.models import Dashboard
+from queryBuilder.models import Query
 
 
 class DashboardAdmin(admin.ModelAdmin):
@@ -10,12 +11,6 @@ class DashboardAdmin(admin.ModelAdmin):
         if not change:
             obj.owner = request.user
         obj.save()
-
-    @staticmethod
-    def queryset(self, request):
-        if request.user.is_superuser:
-            return Dashboard.objects.all()
-        return Dashboard.objects.filter(owner=request.user)
 
     def has_change_permission(self, request, obj=None):
         has_class_permission = \
@@ -28,4 +23,23 @@ class DashboardAdmin(admin.ModelAdmin):
         return True
 
 
+class QueryAdmin(admin.ModelAdmin):
+    exclude = ('owner',)
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.owner = request.user
+        obj.save()
+
+    def has_change_permission(self, request, obj=None):
+        has_class_permission = \
+            super(QueryAdmin, self).has_change_permission(request, obj)
+        if not has_class_permission:
+            return False
+        if obj is not None and not request.user.is_superuser \
+                and request.user.id != obj.author.id:
+            return False
+        return True
+
 admin.site.register(Dashboard, DashboardAdmin)
+admin.site.register(Query, QueryAdmin)
