8f90a1490cd8408c9632f92e23058c86c408023f
bstjohn <bstjohn@pdx.edu>
2015-02-06 16:28:47 -0800

    Skeleton functionality for new query builder created

--- query/views.py
+++ query/views.py
@@ -11,21 +11,46 @@ import time
 import json
 
 
+class Condition:
+    def __init__(self, condition_type, condition_operator, condition_value):
+        self.condition_type = condition_type
+        self.condition_operator = condition_operator
+        self.condition_value = condition_value
+
+    def __str__(self):
+        return self.condition_type + " " + self.condition_operator + " " + str(self.condition_value)
+
+
+class QueryObject:
+    def __init__(self, model_id, creation_date, start_date_time,
+                 end_date_time, stations, conditions, file_name):
+        self.model_id = model_id
+        self.creation_date = creation_date
+        self.start_date_time = start_date_time
+        self.end_date_time = end_date_time
+        self.stations = stations
+        self.conditions = conditions
+        self.file_name = file_name
+
+
 @login_required
 def query_index(request):
     return render(request, 'query/query.html')
 
 form_submitted = False
-
+query_model = Query()
+query_object = QueryObject(None, None, None, None, None, None, None)
 
 # Builds a query given user input
 @login_required
 def query_builder(request):
     global form_submitted
+    global query_model
+    global query_object
     condition_form_set = formset_factory(ConditionForm, extra=1)
     username = None
     creation_date = None
-    query_model = Query()
+    # query_model = Query()
     if request.user.is_authenticated():
         username = request.user.username
         query_model.user_name = username
@@ -38,15 +63,16 @@ def query_builder(request):
         condition_form = condition_form_set(request.POST)
 
         # global form_submitted
-        if signal_form.is_valid() and form_submitted is True and 'send' in request.POST:
-            print(form_submitted.__bool__())
-            # print(convert_to_json(query_model.id, creation_date, start_date_time, end_date_time,
-            #                       stations, conditions, file_name))
+        if signal_form.is_valid() and form_submitted and 'send' in request.POST:
             query_model.save()
+            query_object.model_id = query_model.id
+            print(convert_to_json(query_object))
+            form_submitted = False
 
             return HttpResponseRedirect('/query/query-result/')
         elif 'send' in request.POST:
             return HttpResponseRedirect('/query/query-builder/')
+
         if form.is_valid() and condition_form.is_valid() and 'refresh' in request.POST:
             query_model.owner = request.user
             query_model.query_name = form.cleaned_data['query_name']
@@ -81,8 +107,10 @@ def query_builder(request):
             file_name = file.name
             query_model.file_name = file_name
 
-            # query_model.save()
-            # global form_submitted
+            query_object = QueryObject(None, creation_date,
+                                       start_date_time, end_date_time,
+                                       stations, conditions, file_name)
+
             form_submitted = True
 
             SignalForm.update_signals(signal_form, stations, conditions)
@@ -96,8 +124,15 @@ def query_builder(request):
     return render(request, 'query/query-builder.html', context)
 
 
-def convert_to_json(query_id, creation_date, start_date_time, end_date_time,
-                    stations, conditions, file_name):
+def convert_to_json(query_param):
+    query_id = query_param.model_id
+    creation_date = query_param.creation_date
+    start_date_time = query_param.start_date_time
+    end_date_time = query_param.end_date_time
+    stations = query_param.stations
+    conditions = query_param.conditions
+    file_name = query_param.file_name
+
     voltage_conditions = []
     current_conditions = []
     frequency_conditions = []
@@ -124,13 +159,3 @@ def convert_to_json(query_id, creation_date, start_date_time, end_date_time,
     })
 
     return query
-
-
-class Condition:
-    def __init__(self, condition_type, condition_operator, condition_value):
-        self.condition_type = condition_type
-        self.condition_operator = condition_operator
-        self.condition_value = condition_value
-
-    def __str__(self):
-        return self.condition_type + " " + self.condition_operator + " " + str(self.condition_value)
