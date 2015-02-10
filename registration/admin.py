5eee37183e1ab1ce1180b353f8deaec3e0de385a
Evan <shuping@pdx.edu>
2015-02-09 22:49:53 -0800

    Email uniqe validation has setgit add .

--- registration/admin.py
+++ registration/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from registration.models import Person
+from django.contrib.auth.models import User
 
+# Unique email address
+User._meta.get_field_by_name('email')[0]._unique = True
+Person._meta.get_field_by_name('email')[0]._unique = True
 
 # Register your models here.
 class PersonRegistration(admin.ModelAdmin):
