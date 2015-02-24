13e34c0b7b28257dd9925b243c6e4da0c0744b09
Garrison Jensen <garrison.jensen@gmail.com>
2015-02-24 12:32:59 -0800

    Remove second DATABASES var.

--- bpa/settings.py
+++ bpa/settings.py
@@ -43,17 +43,15 @@ LOGIN_REDIRECT_URL = '/dashboard'
 LOGIN_URL = 'django.contrib.auth.views.login'
 
 # Database configuration
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'bpa',
-        'USER': 'bpa_admin',
-        'PASSWORD': 'NicolaTesa',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 # Application definition
 
@@ -84,17 +82,6 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'bpa.urls'
 
 WSGI_APPLICATION = 'bpa.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
