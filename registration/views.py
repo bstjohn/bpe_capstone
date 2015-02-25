324c6c59494c59138d5905324add9f365663c424
Evan <shuping@pdx.edu>
2015-02-25 11:27:04 -0800

    add some comment and add send create new user messages feature

--- registration/views.py
+++ registration/views.py
@@ -1,28 +1,43 @@
+# User registration view.
+#
+# Bonneville Power Adminstration Front-End
+# Copyright (C) 2015  Shu Ping Chu
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 # views.py is the set of functions that you implement it to describe how to 
 # display the information of your website when we visit the url
 from django.conf import settings
 from django.core.mail import send_mail
 from django.contrib import messages
-
 from django.shortcuts import render
-from django.shortcuts import render_to_response
-from django.http import HttpResponse
-from django.http import HttpResponseRedirect
-from django.contrib import auth
-from django.core.context_processors import csrf
-from django.template import Context, RequestContext, loader
-from django.template.loader import get_template  # get_template fun
-from django.views.generic.base import TemplateView  # how display template
-from registration.forms import MyRegistrationForm  # import from /forms.py
+from django.shortcuts import render_to_response             # allows to render tmeplate back to browser
+from django.http import HttpResponse                        # response to that http
+from django.http import HttpResponseRedirect                # redirect browser to another url
+from django.contrib import auth                             # take care using user login, logout, password
+from django.core.context_processors import csrf             # for security protect
+from django.template import Context, RequestContext, loader # for template language ex: "{{ }}" <--variable inside
+from django.template.loader import get_template             # get_template helper function, to know where the templates are
+from django.views.generic.base import TemplateView          # how display template
+from registration.forms import MyRegistrationForm           # import from /forms.py
 from django.contrib.auth.models import User
-
 from django.contrib.auth.decorators import login_required
 from forms import UserProfileForm
-
 from django.views.generic import ListView, DetailView
 from django.contrib.auth import get_user_model
 from models import UserProfile
-
 
 def register_user(request):
     if request.method == 'POST':
@@ -31,6 +46,7 @@ def register_user(request):
         if form.is_valid():
             save_it = form.save(commit=False)
             save_it.save()
+            messages.add_message(request, messages.SUCCESS, "New user " + save_it.username + " was created")
             # here we can add email to verfiy!!!!
             subject = 'Thank you for register as BPA user from BPA project'
             message = 'Hi ' + save_it.username + ', welcome to use BPA web applictation, we hope you enjoy it!'
@@ -38,7 +54,7 @@ def register_user(request):
             to_list = [save_it.email, settings.EMAIL_HOST_USER]
 
             send_mail(subject, message, from_email, to_list, fail_silently=False)
-            # message.success(request, 'Thanks you for regist BPA user, the email comfirmation sent')
+            messages.success(request, 'Thanks you for regist BPA user, the email comfirmation sent')
             return HttpResponseRedirect('/registration_success/')
         else:
             return HttpResponseRedirect('/registration_fail/')
