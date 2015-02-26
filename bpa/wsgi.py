5b14c3e37cbc22c4366c412612e90e8462cd5982
Garrison Jensen <garrison.jensen@gmail.com>
2015-02-26 00:10:42 -0800

    Add production wsgi file.

--- bpa/wsgi.py
+++ bpa/wsgi.py
@@ -6,11 +6,15 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
+import os, sys, site
 
-import os
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bpa.settings")
-
+# add the virtualenv
+root = os.path.normpath(os.path.join(os.path.dirname(__file__), "../"))
+sys.path.append(root)
+site.addsitedir(os.path.join(root, "env/lib/python3.3/site-packages"))
+site.addsitedir(os.path.join(root, "env/lib64/python3.3/site-packages"))
+ 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bpa.settings_production")
+ 
 from django.core.wsgi import get_wsgi_application
-
 application = get_wsgi_application()
