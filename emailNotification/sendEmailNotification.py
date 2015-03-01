77c22b1b998fcf4ef7d833ccc206254198baa5a2
zalattar <zalattar@users.noreply.github.com>
2015-03-01 12:15:45 -0800

    Update sendEmailNotification.py

--- emailNotification/sendEmailNotification.py
+++ emailNotification/sendEmailNotification.py
@@ -1,3 +1,21 @@
+# Sends an email notification to user specified in arguments
+# Bonneville Power Adminstration Front-End
+# Copyright (C) 2015  Zaynab Alattar
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 # Import smtplib for the actual sending function
 import smtplib
 
@@ -15,7 +33,7 @@ def sendEmailNotification(userName, userEmail):
 	# From == the sender's email address 
 	# To == the recipient's email address
 	msg['Subject'] = 'Notification of Query Result'
-	msg['From'] = 'Zaynab'												# CHANGE ME
+	msg['From'] = 'BPA'												# CHANGE ME
 	msg['To'] = str(userName)
 
 	# Send the message via our own SMTP server, but don't include the
@@ -25,4 +43,4 @@ def sendEmailNotification(userName, userEmail):
 	server.ehlo()
 	server.starttls()
 	server.login('bpa.psucapstone@gmail.com', 'NicolaTesa')
-	server.sendmail('zalattar@pdx.edu', str(userEmail), str(msg))
+	server.sendmail('bpa.psucapstone@gmail.com', str(userEmail), str(msg))
