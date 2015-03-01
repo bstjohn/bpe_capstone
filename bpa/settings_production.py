6f7547b1a429acc5e0ea35351113176bd88bbf8d
Garrison Jensen <garrison.jensen@gmail.com>
2015-02-28 22:40:25 -0800

    Add production settings file.

--- bpa/settings_production.py
+++ bpa/settings_production.py
@@ -1,19 +1,119 @@
-# settings_production.py
+"""
+Django settings for bpe_capstone project.
 
-from settings import *
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
 
-DEBUG = TEMPLATE_DEBUG = True
-DATABASE_NAME = 'bpa'
-DATABASE_USER = 'bpa'
-DATABASE_PASSWORD = 'bpa'
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+# Sending email for gmail or google apps
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'bpa.psucapstone@gmail.com'
+EMAIL_HOST_PASSWORD = 'NicolaTesa'
+EMAIL_PORT = 587
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = ''
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+TEMPLATE_DEBUG = False
+
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates'),
+                 os.path.join(BASE_DIR, 'templates/user')]
+     
+
+ALLOWED_HOSTS = ['*']
+
+# Login settings
+
+LOGIN_REDIRECT_URL = '/dashboard'
+LOGIN_URL = 'django.contrib.auth.views.login'
+
+# Database configuration
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'bpa',
-            'USER': 'bpa',
-            'PASSWORD': 'bpa',
-            'HOST': '127.0.0.1',
-            'PORT': '5432',
+            'NAME': '',
+            'USER': '',
+            'PASSWORD': '',
+            'HOST': '',
+            'PORT': '',
             }
         }
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'info',
+    'dashboard',
+    'query',
+    'registration',
+    'stations',
+)
+
+# Send user messages
+TEMPLATE_CONTEXT_PROCESSORS = {
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.request',  
+    'django.contrib.messages.context_processors.messages',
+}
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'bpa.urls'
+
+WSGI_APPLICATION = 'bpa.wsgi.application'
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+    '/static/general/',
+)
