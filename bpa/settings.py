7cf037ae451da6064cb5b6242a1b1f4df02bfeb6
Evan <shuping@pdx.edu>
2015-03-05 11:16:20 -0800

    change email passowrd setting

--- bpa/settings.py
+++ bpa/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'bpa.psucapstone@gmail.com'
-EMAIL_HOST_PASSWORD = 'NicolaTesa'
+EMAIL_HOST_PASSWORD = '***'
 EMAIL_PORT = 587
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
